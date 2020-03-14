5cb1c18582dd39f1c77340dd03aa92770074949b
Serkan Tunc <serkantuncofficial@gmail.com>
2020-03-14 16:11:09 +0300

    -

---
+++ Templates/script.js
@@ -0,0 +1,9 @@
+//Hierarchy-CLI
+//Author by Serkan Tunc
+//STANDART JS
+
+function main(){
+    alert("Hierarchy-CLI");
+}
+
+main();
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = (num1, num2) => {
-    return num1 + num2;
-};
-//# sourceMappingURL=index.js.map
